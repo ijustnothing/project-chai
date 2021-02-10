184b8290ed47cb8c9ba15460d4d1e6c3963b586b
inothing <inothing1087@gmail.com>
2021-02-10 14:15:58 +0300

    fix post

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const postSchema = new Schema({
-    text:String
+    text:String,
+    author:[{ type: mongoose.Types.ObjectId, ref: 'users' }],
 })
 
 module.exports = mongoose.model('posts', postSchema)
