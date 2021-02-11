b8fcd225d95186d57fda146d0a913ad98c137069
inothing <inothing1087@gmail.com>
2021-02-11 11:48:12 +0300

    test version

--- controllers/cabinet.js
+++ controllers/cabinet.js
@@ -17,13 +17,6 @@ module.exports.cabinet_get = async function (req, res) {
   console.log(teas);
   res.render('cabinet', { teas });
 };
-module.exports.cabinet_post = async function (req, res) {
-  const { name, location, text } = req.body;
-  console.log(name, location, text);
-  const tea = new Tea({ name, location, text });
-  await tea.save();
-  res.json(tea);
-};
 module.exports.cabinet_delete = async function (req, res) {
   const { id } = req.params;
   await Tea.findByIdAndDelete({ _id: id });
--- routes/cabinet.js
+++ routes/cabinet.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const controller = require('../controllers/cabinet');
 const router = express.Router();
-const {requireAuth} = require('../middleware/authMiddleware')
+const { requireAuth } = require('../middleware/authMiddleware');
+const Tea = require('../models/Tea')
 
 router.get('/', requireAuth, controller.cabinet_get);
-router.post('/', controller.cabinet_post);
+router.post('/', async function (req, res) {
+  const { name, location, text } = req.body;
+  console.log(name, location, text);
+  const tea = new Tea({ name, location, text });
+  await tea.save();
+  res.json(tea);
+});
 router.delete('/:id', controller.cabinet_delete);
 
 module.exports = router;
